3963bd4a1d84386cfc8aede820e7f97b2f3f4277
preethi12-tes <preethit@testvagrant.com>
2025-02-14 15:43:21 +0530

    added Home test case

--- pages/homePage.js
+++ pages/homePage.js
@@ -3,6 +3,13 @@ import { ApiTestingPage } from "./apiTestingPage";
 export class HomePage {
     constructor(page) {
         this.page = page
+        this.menuOptions=page.locator(`.nav.navbar-nav li a`)
+        this.carsoul=page.locator(`#slider-carousel .carousel-inner .item`)
+        this.categories=page.locator(`.panel.panel-default .panel-heading .panel-title`)
+        this.brandList=page.locator(`.brands-name ul li a`)
+        this.featuredItem=page.locator(`.single-products`)
+        this.emailSubsciptionBox=page.locator(`#susbscribe_email`)
+        this.subscribeButton=page.locator(`#subscribe`)
     }
     async navToHome()
     {
@@ -13,5 +20,34 @@ export class HomePage {
         await this.page.locator(`//a[normalize-space()='API Testing']`).click()
         return new ApiTestingPage(this.page)
     }
+    async getMenuOptions()
+    {
+        const menuOption = await this.menuOptions.allTextContents()
+        return menuOption
+    }
+    async getCarsoulCount(){
+        const carsoulCount = await this.carsoul.count()
+        return carsoulCount
+    }
+    async getCategories(){
+        const categoriesCount= await this.categories.count()
+        return categoriesCount
+    }
+    async getBrandList(){
+       const brandList= await this.brandList.count()
+       return brandList
+    }
+    async getFeaturedItems()
+    {
+        const featuredItem= await this.featuredItem.count()
+        return featuredItem
+    }
+    async getFooterSection()
+    {
+       const emailBox=await this.emailSubsciptionBox.isVisible()
+       const subscribeBtn=await this.subscribeButton.isVisible()
+       return {emailBox,subscribeBtn}
+    }
+
     
 }
---
+++ src/Home.spec.js
@@ -0,0 +1,54 @@
+    import { test, expect, request } from '@playwright/test';
+    import { HomePage } from '../pages/homePage';
+    import { log } from 'console';
+
+    test.describe('Home Page Verification in Web',()=>{
+        let homepage
+        test.beforeEach(async ({ page }) => {
+            homepage = new HomePage(page);
+            await homepage.navToHome();
+        });
+    test('To check Navigation Menu in Home Page', async ({ page,request }) => {
+        
+        const list=await homepage.getMenuOptions()
+        const cleanedMenuOptionTexts = list.map(item => 
+            item.trim()  
+                .toLowerCase() 
+                .replace(/[^a-z0-9\s]/g, '')  
+                .replace(/\s+/g, ' ')  
+        );
+        const expectedMenuOptions = [
+            'home', ' products', 'cart', 'signup login', 
+            'test cases', 'api testing', 'video tutorials', 'contact us'
+        ];
+    
+        expect(cleanedMenuOptionTexts).toEqual(expectedMenuOptions);
+    });
+    test('To Veify Carsoul slides count',async() =>{
+
+        const carsoulCount=await homepage.getCarsoulCount()
+        expect(carsoulCount).toEqual(3)
+    })
+    test('To Veify Categories count',async() =>{
+
+        const categoriesCount=await homepage.getCategories()
+        expect(categoriesCount).toEqual(3)
+    })
+    test('To Veify Brand Section ',async() =>{
+
+        const brandCount = await homepage.getBrandList();
+        expect(brandCount).toEqual(8)
+    })
+    test('To Veify Featured Items ',async() =>{
+
+        const featuredItem = await homepage.getFeaturedItems();
+        expect(featuredItem).toBeGreaterThan(1)
+    })
+    
+    test('To Veify Footer Sectioon is visible ',async() =>{
+
+        expect(await homepage.getFooterSection()).toBeTruthy
+    })
+    
+
+})
